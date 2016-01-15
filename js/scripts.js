12bd4570b752b9b92ab5e6020098536d92d8f588
Brad London <london.brad@gmail.com>
2016-01-15 11:26:33 -0800

    fix the spelling of honolulu to be correct

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,37 @@
 $(document).ready(function(){
 
   $("form#blanks").submit(function(event){
+
+
     var temp = $("input[name='tempOptions']:checked").val();
     var citytype = $("input[name='cityTypeOptions']:checked").val();
     var name = $("input#inputName").val();
     var age = $("input#inputAge").val();
     var color = $("input#inputColor").val();
-console.log(age)
-console.log(name)
-console.log(color)
+
+
     if (age < 18) {
       var destination = "Disney World";
 
     } else {
       if (temp === 'warm'){
         if (citytype === 'urban'){
-          var destination = "Honalulu";
+          var destination = "Honolulu";
         } else {
           var destination = "Fiji";
         }
 
 
-    } else {
-      if (citytype === 'urban'){
-        var destination = "Moscow";
       } else {
-        var destination = "Nuuk, Greenland";
+        if (citytype === 'urban'){
+          var destination = "Moscow";
+        } else {
+          var destination = "Nuuk, Greenland";
+        }
       }
     }
-  }
 
 
-
-
-
-console.log(destination)
     $(".results").show();
     $(".vacationSelection").text(destination).css("color", color);
     $(".nameGiven").text(name);
