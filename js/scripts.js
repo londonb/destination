6bd26ec22896b203c1052e6717176e7f4a39e0f0
Brad London <london.brad@gmail.com>
2016-01-15 10:04:13 -0800

    recognize input in js

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  $("form#blanks").submit(function(event){
+    var temp = $("input[name='tempOptions']:checked").val();
+    var citytype = $("input[name='cityTypeOptions']:checked").val();
+    var name = $("input#inputName").val();
+    var age = $("input#inputAge").val();
+    var color = $("input#inputColor").val();
+console.log(age)
+console.log(name)
+console.log(color)
+    if (age < 18) {
+      var destination ="Disney World";
+    }
+
+
+console.log(destination)
+    $(".results").show();
+    $(".vacationSelection").text(destination);
+    event.preventDefault();
+  });
+});
